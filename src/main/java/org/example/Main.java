b6718028e847aab09c45c846fd94baf30467023e
Daniel MC <danielcarrillo3200@gmail.com>
2024-10-24 12:40:17 -0500

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,8 @@ public class Main {
 		Producto camiseta = new Producto("Camiseta", 45000.0, 20);
 		Producto pantalon = new Producto("Pantalon", 130000.0, 10);
 		Producto zapato = new Producto("Zapato", 250000.0, 5);
+		Producto adasdas = new Producto("Adidas", 2321132.1, 12);
+		Producto adios = new Producto("Adidas", 213412.0,412);
 
 		ProductoDAO productoDAO = new ProductoDAO(BaseDeDatos.H2);
 
