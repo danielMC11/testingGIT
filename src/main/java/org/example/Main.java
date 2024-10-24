034e548695aee8a242341f2ebf101216aaa87a0b
Daniel MC <danielcarrillo3200@gmail.com>
2024-10-24 15:06:27 -0500

    2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,15 +7,13 @@ import org.example.modelo.Producto;
 public class Main {
 	public static void main(String[] args) {
 
-		Producto camiseta = new Producto("Camiseta", 45000.0, 20);
-		Producto pantalon = new Producto("Pantalon", 130000.0, 10);
-		Producto zapato = new Producto("Zapato", 250000.0, 5);
 		Producto adasdas = new Producto("Adidas", 2321132.1, 12);
 		Producto adios = new Producto("Adidas", 213412.0,412);
+		Producto neki = new Producto("Nike", 21312.0, 123);
 
 		ProductoDAO productoDAO = new ProductoDAO(BaseDeDatos.H2);
 
-		for(Producto producto : productoDAO.	buscarTodos())
+		for(Producto producto : productoDAO.buscarTodos())
 			System.out.println(producto);
 
 	}
