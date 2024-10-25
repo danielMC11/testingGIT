1becdc1b02bc46d2f72ffe497fe2c94de51997c5
DanielMC <danielcarrillo3200@gmail.com>
2024-10-25 17:09:11 -0500

    los kike

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,7 @@ public class Main {
 		Producto z1 = new Producto("Adasdas", 20.0, 12);
 		Producto z2 = new Producto("aDIOS", 15.0,412);
 		Producto z3 = new Producto("NEKI", 50.0, 124);
+		Producto z4 = new Producto("KIKE", 12.0, 12);
 
 		ProductoDAO productoDAO = new ProductoDAO(BaseDeDatos.SQLITE);
 
