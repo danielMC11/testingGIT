a8a16dd8d6eeae88f96f992976b2011020fdd2e8
Daniel MC <danielcarrillo3200@gmail.com>
2024-10-25 21:20:14 -0500

    waza

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,8 +9,9 @@ public class Main {
 
 		Producto z1 = new Producto("Adasdas", 20.0, 12);
 		Producto z2 = new Producto("aDIOS", 15.0,412);
-		Producto z3 = new Producto("NEKI", 50.0, 124);
+		Producto z2point2 = new Producto("Adormir", 23.0, 125);
 		Producto z4 = new Producto("KIKE", 12.0, 12);
+		Producto z5 = new Producto("KEVIN", 15.0,52);
 
 		ProductoDAO productoDAO = new ProductoDAO(BaseDeDatos.SQLITE);
 
